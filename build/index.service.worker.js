c6917460646b4a3e5cb246c24f3ed221c43b5b04
k0rae <amazingtonman@gmail.com>
2025-09-21 02:31:11 +0700

    test localstorage

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758394519|2797742382';
+const CACHE_VERSION = '1758396638|4917336966';
 /** @type {string} */
 const CACHE_PREFIX = 'Carassius-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- godot/kliker.gd
+++ godot/kliker.gd
@@ -3,11 +3,15 @@ extends Control
 @onready var label: Label = $Label2
 @onready var button: Button = $Button
 
-@onready var counter = 0
+var counter = 0
 
 func _ready() -> void:
+	if OS.has_feature('web'):
+		var saved = JavaScriptBridge.eval("+localStorage.getItem('count')")
+		counter = saved
 	button.pressed.connect(_button_pressed)
 	
 func _button_pressed():
 	counter += 1
-	label.text = str(counter)
+	JavaScriptBridge.eval("localStorage.setItem('count', %d);" % counter)
+	label.text = str(int(counter))
