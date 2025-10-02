d2540a7afc5b3ae02468c35736567a1a27dbb141
k0rae <amazingtonman@gmail.com>
2025-10-03 02:46:33 +0700

    test window stretch mode

--- server/public/index.service.worker.js
+++ server/public/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759426137|1250158949';
+const CACHE_VERSION = '1759434262|106606910';
 /** @type {string} */
 const CACHE_PREFIX = 'Carassius-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
