7aec8157483893396e6543d41845f6f988162586
k0rae <amazingtonman@gmail.com>
2025-10-03 02:53:25 +0700

    fix fix

--- server/public/index.service.worker.js
+++ server/public/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759434262|106606910';
+const CACHE_VERSION = '1759434691|535812188';
 /** @type {string} */
 const CACHE_PREFIX = 'Carassius-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
